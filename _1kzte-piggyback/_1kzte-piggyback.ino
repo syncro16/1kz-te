db4bd4a569ef188bfff06a034071f69ee24eee26
Juho Pesonen <syncro16@outlook.com>
2020-05-29 10:24:21 +0300

    Fixed typo, improved enrichment calculation

--- _1kzte-piggyback/_1kzte-piggyback.ino
+++ _1kzte-piggyback/_1kzte-piggyback.ino
@@ -3,7 +3,7 @@
  * 
  * Modifies (expands) spill of solenoid pulse according to analog input (MAP sensor for example) to allow more fueling
  * 
- * This is code for Arduino Uno
+ * This is code for Arduino Mega / Uno
  */
 
 
@@ -12,7 +12,7 @@ volatile uint16_t injectionDuration;
 volatile uint16_t newInjectionDuration;
 volatile uint16_t fullCycleDuration;
 volatile uint16_t check;
-volatile uint8_t enrichment = 100;
+volatile uint8_t enrichment = 0;
 
 #define INPUT_PIN 2
 #define OUTPUT_PIN 7
@@ -26,7 +26,7 @@ void setup() {
 	pinMode(INPUT_PIN,INPUT);
 	pinMode(OUTPUT_PIN,OUTPUT);
 	pinMode(ANALOG_ADJUST_PIN, INPUT);
-	attachInterrupt(0, rpmTrigger, CHANGE);  // 0 == Input #2 on arduino uno
+	attachInterrupt(digitalPinToInterrupt(INPUT_PIN), rpmTrigger, CHANGE);  // 0 == Input #2 on arduino uno
 
 	// https://circuitdigest.com/microcontroller-projects/arduino-timer-tutorial
 	// timer 1 prescaler set to 64 = 250 000Hz
@@ -50,29 +50,33 @@ void rpmTrigger() {
 	} else {
 		// state is high, ECU operated solenoid turned OFF	
 		injectionDuration = TCNT1;
-		if (enrichment) {
+		if (enrichment > 0) {
 			// Wait until TCNT1 passes calculated enrichment amount
 			// Enrichment amount is calculated with fixed point wizardry
-			uint32_t adjust = ((uint32_t)injectionDuration<<8)/(256-enrichment);
+
+			// this is more aggressive curve
+			// uint32_t adjust = ((uint32_t)injectionDuration * enrichment) >> 8;
+			
+			uint32_t adjust = ((uint32_t)injectionDuration * enrichment) >> 9;
 
 			// Do not expand adjust over full cycle, because we miss next interrupt
-			
 			if (adjust < fullCycleDuration-100) {
 				// It is generally bad idea to "jam" interrupt handler
 				// but is ok for this purpose. 
-				while (TCNT1<adjust) 
+				while (TCNT1 < injectionDuration+adjust) 
 					__asm("nop");
 			}
 		}
-		
 		digitalWrite(OUTPUT_PIN,LOW);
-		newInjectionDuration = TCNT1;
+		newInjectionDuration = TCNT1;				
 	}
 }
 
 // This called when interrupt overflow happends (no RPM input -> declare engine as stopped)
 ISR(TIMER1_OVF_vect) {
 	fullCycleDuration = 0xffff;
+	newInjectionDuration = 0;
+	injectionDuration = 0;
 	digitalWrite(OUTPUT_PIN,LOW);
 }
 
@@ -92,19 +96,26 @@ void loop() {
 	uint16_t val = analogRead(ANALOG_ADJUST_PIN);
 	// map input voltages from ~2v to ~4v enrichment range. 
 	// Probably works fine for 3bar map (we dont care first 1bar range, because that's below atmosphere pressure)
-	enrichment = map(val,406,819,0,255); 	
-	
+
+	int16_t eVal = map(val,406,819,0,255); 
+	if (eVal < 0) 
+		eVal = 0;
+	if (eVal > 255) 
+		eVal = 255;
+	enrichment = eVal;
+		
 	// generate reference pulse to pin13 (led). 
 	// This is 1000rpm (16.66r/s = 60ms) - if there is one trigger per 360° rotation
-	// Because there is 4 triggers per rotation, and pump rotates two times faster than engine
+	// Because there is 4 triggers per rotation, and pump rotates two times slower than engine
 	// so the measured reference RPM should be about 500rpm
 	
 	char buf[60];
 	
 	digitalWrite(13,LOW);
-	delay(6);
+	delay(46);
 	digitalWrite(13,HIGH);
-	delay(54);
+	delay(24);
+	
 	sprintf(buf,"RPM:%4d, injectionDuration: %4d->%4d, enrichment:%3d",getRPM(),injectionDuration,newInjectionDuration,enrichment);
 	Serial.println(buf);
 
